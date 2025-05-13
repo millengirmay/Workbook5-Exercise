9b6f1f5c430b44d43f6a3f555650463ee4970d44
Student <millenkahsaygirmay@gmail.com>
2025-05-13 15:00:29 -0700

    Initial commit

--- src/main.java
+++ src/main.java
@@ -1,13 +1,21 @@
 public class main {
     public static void main(String[] args) {
-        Moped slowRide = new Moped();
+        /*Moped slowRide = new Moped();
         slowRide.setColor("Red");
         slowRide.setFuelCapacity(5);
-        System.out.println("Moped color: " + slowRide.getColor());
+        System.out.println("Moped color: " + slowRide.getColor());*/
 
-        Car SUV = new Car();
+        /*Car SUV = new Car();
         SUV.setNumberOfPassengers(5);
-        System.out.println("Number of passenger: " + SUV.getNumberOfPassengers());
+        System.out.println("Number of passenger: " + SUV.getNumberOfPassengers());*/
+
+        //Constructor with arguments
+
+        Moped slowride = new Moped("red", 4, 60, 12);
+        System.out.println(slowride.getCargoCapacity());
+
+        SemiTruck truck = new SemiTruck("White", 2, 120, 20);
+        System.out.println(truck.getNumberOfPassengers());
 
 
     }
