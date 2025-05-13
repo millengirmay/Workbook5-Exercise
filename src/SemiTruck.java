5bd1c0fb93dcb15697160742a37e6cb261476eba
Student <millenkahsaygirmay@gmail.com>
2025-05-13 14:54:17 -0700

    Initial commit

--- src/SemiTruck.java
+++ src/SemiTruck.java
@@ -1,6 +1,5 @@
 public class SemiTruck extends Vehicle{
     public SemiTruck(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity ){
         super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
-
     }
 }
