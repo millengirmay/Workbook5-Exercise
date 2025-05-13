5b26d13a1eac134f075cad0898d475c912178d2a
Student <millenkahsaygirmay@gmail.com>
2025-05-13 14:52:41 -0700

    Initial commit

--- src/Hovercraft.java
+++ src/Hovercraft.java
@@ -1,3 +1,6 @@
 public class Hovercraft extends Vehicle{
+    public Hovercraft(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity ){
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
 
 }
